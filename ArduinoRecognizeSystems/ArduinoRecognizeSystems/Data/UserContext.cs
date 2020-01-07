3355fa2ca451a37581cc6fd0becd736274cda83d
dreinix <ricardo.aar@outlook.com>
2020-01-06 22:14:27 -0400

    DataCreation

---
+++ ArduinoRecognizeSystems/ArduinoRecognizeSystems/Data/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using ArduinoRecognizeSystems2.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ArduinoRecognizeSystems2.Data
+{
+    class UserContext: DbContext
+    {
+
+        public UserContext()
+        {
+            SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-V2MPCEE\XINISERVER;Initial Catalog=ARSDB;
+                                                                +User ID=ARSUser;Password=ars123");
+        }
+    }
+}
---
+++ ArduinoRecognizeSystems/ArduinoRecognizeSystems/Model/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ArduinoRecognizeSystems2.Model
+{
+    class Usuario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Necesita un Usuario")]
+        [MinLength(3), MaxLength(30)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Necesita una contraseña")]
+        [MinLength(4), MaxLength(20)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Necesita un Nombre")]
+        [MinLength(3), MaxLength(30)]
+        public string Name { get; set; }
+    }
+}
